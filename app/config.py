ef2816df91cbe748aa8bc1d2e0291197fcddb797
brentnequin <brentnequin@gmail.com>
2022-08-15 18:15:19 -0500

    stop nonetype error

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1) or 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "").replace("postgres://", "postgresql://", 1) or 'sqlite:///' + os.path.join(basedir, 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
